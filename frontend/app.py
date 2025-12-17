17d0159bfd4acd4e022a46ee0a277738eb8fc662
Musmannazir <nazirusman127@gmail.com>
2025-12-17 12:58:23 +0500

    Fix 403 Error by using localhost

--- frontend/app.py
+++ frontend/app.py
@@ -250,7 +250,7 @@ if page == "Forecast Inflation":
             
             try:
                 # UPDATED: Pointing to your new Render Backend
-                api_url = "https://inflation-backend-6.onrender.com/predict"
+                api_url = "http://localhost:8000/predict"
                 
                 response = requests.post(api_url, json=payload, timeout=15)
                 
